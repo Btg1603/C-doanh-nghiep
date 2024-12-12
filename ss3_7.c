4c0fafa3944c79ae72dda8231e8751a26650dbe0
Btg1603 <gbuif12@gmail.com>
2024-12-12 17:40:52 +0700

    Update ss3_7.c

--- ss3_7.c
+++ ss3_7.c
@@ -3,10 +3,17 @@ int main(){
 	int N,temp,t=0,i;
 	printf("Nhap so nguyen co 4 chu so bat ki ");
 	scanf("%d",&N);
-	while(N!=0){
 	temp=N%10;
 	N/=10; 
 	t+=temp;
-}
+	temp=N%10;
+	N/=10; 
+	t+=temp;
+	temp=N%10;
+	N/=10; 
+	t+=temp;
+	temp=N%10;
+	N/=10; 
+	t+=temp;
 	printf("tong = %d", t);   
 } 
