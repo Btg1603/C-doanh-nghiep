080eb3b6f78042ce2d8ddfcc6273bc2bc957cf3c
Btg1603 <gbuif12@gmail.com>
2024-12-12 17:42:59 +0700

    Update ss3_8.c

--- ss3_8.c
+++ ss3_8.c
@@ -2,16 +2,20 @@
 int main() {
     int n, reversedNum = 0, remainder;    
     printf("Nhap mot so nguyen co bon chu so: ");
-    scanf("%d", &n);
-    if (n < 1000 || n > 9999) {
-        printf("vui long nhap mot so co bon chu so\n");
-        return 1;
-    }
-    while (n != 0) {
+    scanf("%d", &n);   
         remainder = n % 10;          
         reversedNum = reversedNum * 10 + remainder;
-        n /= 10;                     
-    }
+        n /= 10; 
+      remainder = n % 10;          
+        reversedNum = reversedNum * 10 + remainder;
+        n /= 10;    
+      remainder = n % 10;          
+        reversedNum = reversedNum * 10 + remainder;
+        n /= 10;    
+      remainder = n % 10;          
+        reversedNum = reversedNum * 10 + remainder;
+        n /= 10;    
+    
     printf("so nghich dao la: %d\n", reversedNum);
     return 0;
 }
