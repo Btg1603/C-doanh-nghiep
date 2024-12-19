e653a7169a8e7b3052688bf6072778d7fbb5452b
Btg1603 <gbuif12@gmail.com>
2024-12-19 09:33:04 +0700

    Update ss7_3.c

--- ss7_3.c
+++ ss7_3.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main(){	
-	int arr[5],t=0;
+	int arr[5],flag=0;
 	for (int i = 0 ; i < 5 ; i++){
 		printf("nhap phan tu thu %d = ",i+1);
 		scanf("%d",&arr[i]); 	
 	}
 	printf("Cac phan tu so chan trong mang la:\n"); 
 	for (int i = 0; i < 5; i++) {
-	if (arr[i] % 2==0){ 
+	if (arr[i] % 2==0){
+	flag=1;
 	printf("%d\n",arr[i]);
-	t+=1; 
-	}else if(t==0) {
+	 
+	}else if(flag!=1) {
 	printf("khong co so chan "); 
 	return 0; 
 }
