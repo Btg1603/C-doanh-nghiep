9317e36a596f165dff1ec6481d1ac2ad334828e5
Btg1603 <gbuif12@gmail.com>
2024-12-20 08:13:01 +0700

    Update ss8_2.c

--- ss8_2.c
+++ ss8_2.c
@@ -5,7 +5,7 @@ int main(){
 	printf("nhap phan tu muon tim kiem: ");
 	scanf("%d",&l); 
 	for (int i = 0 ; i < n ; i++){
-	if(l=arr[i]) printf("vi tri phan tu trong mang la arr[%d]",i);
+	if(l==arr[i]) printf("vi tri phan tu trong mang la arr[%d]",i);
 	else	printf ("phan tu khong ton tai trong mang");
 }
 } 
