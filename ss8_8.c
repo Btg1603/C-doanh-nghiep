4c52e3736c2b3cf11a9bee36701d58584957fdfb
Btg1603 <gbuif12@gmail.com>
2024-12-20 08:24:03 +0700

    Update ss8_8.c

--- ss8_8.c
+++ ss8_8.c
@@ -3,30 +3,29 @@
 int main() {
     int n;
 
-    printf("Nhap so hang va cot ");
+    printf("Nhập số hàng và cột của ma trận vuông: ");
     scanf("%d", &n);
 
     int matrix[n][n];
 
+    // Nhập giá trị cho ma trận
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            printf("Nhap gia tri cho phan tu [%d][%d]: ", i, j);
+            printf("Nhập giá trị cho phần tử [%d][%d]: ", i, j);
             scanf("%d", &matrix[i][j]);
         }
     }
-    int temp=0; 
-     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++){			 
-            if (i+j==n-1){
-            		printf("%d ", matrix[i][j]);
-            	temp+=matrix[i][j]; 
-			}else printf("  "); 
-        }
-        printf("\n"); 
+
+    int temp = 0;
+
+    // In và tính tổng đường chéo phụ
+    printf("Các phần tử trên đường chéo phụ là:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", matrix[i][n - i - 1]); // Phần tử trên đường chéo phụ
+        temp += matrix[i][n - i - 1];       // Tính tổng
     }
-    
-    printf("tong gia tri duong cheo phu la %d",temp);
+
+    printf("\nTổng giá trị đường chéo phụ là: %d\n", temp);
 
     return 0;
 }
-
