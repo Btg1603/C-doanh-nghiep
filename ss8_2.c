add702d0114223acef03154792c682a4523ecaa4
Btg1603 <gbuif12@gmail.com>
2024-12-20 08:16:23 +0700

    Update ss8_2.c

--- ss8_2.c
+++ ss8_2.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main(){
-	int n,l;
+	int n,l,flag=0;
 	int arr[5]={1,2,3,4,5};
 	printf("nhap phan tu muon tim kiem: ");
 	scanf("%d",&l); 
 	for (int i = 0 ; i < n ; i++){
-	if(l==arr[i]) printf("vi tri phan tu trong mang la arr[%d]",i);
-	else	printf ("phan tu khong ton tai trong mang");
+	if(l==arr[i])
+	printf("vi tri phan tu trong mang la arr[%d]",i);
+	flag=1;
 }
+	 if (!flag) {
+        printf("Phần tử không tồn tại trong mảng.\n");
 } 
