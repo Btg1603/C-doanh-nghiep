c1cbddba06d66642d0460f4cfb1bb72b3e61e5fc
Btg1603 <gbuif12@gmail.com>
2024-12-21 07:45:11 +0700

    Update ss6_10.c

--- ss6_10.c
+++ ss6_10.c
@@ -1,19 +1,20 @@
 #include <stdio.h> 
 
 int main() {
-	int n;
+	int flag=1,n;
 	printf("nhap so nguyen bat ki ");
 	scanf("%d",&n);
-	if (n<1){
+	if (n<2){
 		printf("khong phai so nguyen to");
 		}else {
 			for (int i=2;i*i<=n;i++){
 				if (n % i == 0) 
-				printf ("khong la so nguyen to"); 
-				else printf ("la so nguyen to");
-				return 0;13  
+				flag=0;
+				break;  
 			} 
 		}
+	if(flag) printf("la so nguyen to");
+	else printf("khong la so nguyen to");
 		return 0;  
 	} 
 
